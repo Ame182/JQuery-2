f7f5a1d551a774db69034eff1dd4210291b89fbf
Amelia Fayad <ame@MBP-de-Amelia.iptv.microsoft.com>
2019-01-12 19:48:45 -0300

    funcional

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 $(function(){
-	$('.crear').click(function(){
-		var tweet = $('.texto').val();
+	$('.myform').on('submit' , function(event){
+		var text= $(".texto").val();
 		event.preventDefault();
-		console.log(tweet);
-
-	$('.derecha').append('<div class="rellena">'+ tweet + '</div>')
-		$('.tweet').val('');
-		$('.tweet').focus();
-		$('.eltweet').html('<p>' + tweet + '</p>');
-	});
+		 
+		$('input').click(function(e) {
 		
+		var $newTweet = $('.demotweet').clone();
+	    $newTweet.find('.contenido-tweet').html('<p>' + text + '</p>').val();
+	    $('.derecha').prepend($newTweet);
+	    e.preventDefault();
+
+		});
 });
 
-		
+	  $('body').on('click', '.like', function(){
+	    var likes = parseInt($(this).parent().find('.contador-likes').html());
+	    likes++;
+	    $(this).parent().find('.contador-likes').html(likes);
 	
+	    if (likes == 1) {
+	
+	      $(this).parent().find('.like').addClass('like--rojo');
+	    };
+	});
+
+  $('body').on('click', '.eliminar', function (e) {
+	    e.preventDefault();
+	    $(this).parent().parent().fadeOut();
+	
+	  });
+});
